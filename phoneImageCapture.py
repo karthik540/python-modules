aaac9fc3f49fbd1cd3cc1bc91a92ac757af96843
karthik540 <36918151+karthik540@users.noreply.github.com>
2019-07-07 12:24:20 +0530

    phone Image Capture added

---
+++ phoneImageCapture.py
@@ -0,0 +1,17 @@
+import urllib.request
+import cv2
+import numpy as np
+import time
+
+url = "http://192.168.1.4:8080/shot.jpg?rnd=916450"
+
+while(True):
+    last_time = time.time()
+    resp = urllib.request.urlopen(url)
+    imgNp = np.asarray(bytearray(resp.read()) , dtype=np.uint8)
+    img = cv2.imdecode(imgNp , -1)
+    cv2.imshow('test' , img)
+    print('FPS: {}'.format(1/(time.time() - last_time)))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
